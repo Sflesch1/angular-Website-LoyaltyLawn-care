ada315c501aea1f1722e8e5775a3acdd8590ade5
sflesch1 <sean_flesch@yahoo.com>
2025-10-07 15:53:58 -0500

    Describe your changes here

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB9vLTf7HP0GoP7KOkIyZ-6kz6PFuTW3Ws",
+    authDomain: "backend-for-loyalty-lawncare.firebaseapp.com",
+    projectId: "backend-for-loyalty-lawncare",
+    storageBucket: "backend-for-loyalty-lawncare.appspot.com", // <-- must end with .appspot.com
+    messagingSenderId: "36958288907",
+    appId: "1:36958288907:web:a132f4c05f966767ac5b0e",
+    measurementId: "G-QS26KES5G5"
+  }
+};
